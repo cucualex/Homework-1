a405bed1e0fcbac592a35d99525114add0374b24
cucualex <cucualexandru15@yahoo.com>
2023-07-24 19:28:36 +0300

    Update(The form work correct, images, completed, delete buttons)
    localStorage don t store and need more styling for tasks and buttons

--- homework-1/index.js
+++ homework-1/index.js
@@ -1,29 +1,98 @@
-const showButton = document.getElementById("showDialog");
-const favDialog = document.getElementById("favDialog");
-const outputBox = document.querySelector("output");
-const selectEl = favDialog.querySelector("select");
-const confirmBtn = favDialog.querySelector("#confirmBtn");
+const addTaskButton = document.getElementById("addTaskButton");
+const taskFormDialog = document.getElementById("taskFormDialog");
+const taskForm = document.getElementById("taskForm");
+const taskList = document.getElementById("taskList");
 
-// "Show the dialog" button opens the <dialog> modally
-showButton.addEventListener("click", () => {
-  favDialog.showModal();
+addTaskButton.addEventListener("click", () => {
+  taskForm.reset();
+  taskFormDialog.showModal();
 });
 
-// "Favorite animal" input sets the value of the submit button
-selectEl.addEventListener("change", (e) => {
-  confirmBtn.value = selectEl.value;
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const taskTitle = document.getElementById("taskTitle").value;
+  const taskDescription = document.getElementById("taskDescription").value;
+  const assigneeEmail = document.getElementById("assigneeEmail").value;
+
+  if (
+    taskTitle.trim() !== "" &&
+    taskDescription.trim() !== "" &&
+    assigneeEmail.trim() !== ""
+  ) {
+    const taskItem = createTaskItem(taskTitle, taskDescription, assigneeEmail);
+    taskList.appendChild(taskItem);
+    taskFormDialog.close();
+  }
 });
 
-// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
-favDialog.addEventListener("close", (e) => {
-  outputBox.value =
-    favDialog.returnValue === "default"
-      ? "No return value."
-      : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
+document.getElementById("cancelBtn").addEventListener("click", () => {
+  taskFormDialog.close();
 });
 
-// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
-confirmBtn.addEventListener("click", (event) => {
-  event.preventDefault(); // We don't want to submit this fake form
-  favDialog.close(selectEl.value); // Have to send the select box value here.
+function addTaskToList(title, description, assignee) {
+  const taskItem = document.createElement("li");
+  taskItem.innerHTML = `
+      <strong>${title}</strong>
+      <p>${description}</p>
+      <p>Assignee: ${assignee}</p>
+  `;
+  taskList.appendChild(taskItem);
+}
+function saveTaskToLocalStorage(task) {
+  const tasks = getTasksFromLocalStorage();
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function getTasksFromLocalStorage() {
+  const tasksJSON = localStorage.getItem("tasks");
+  return tasksJSON ? JSON.parse(tasksJSON) : [];
+}
+function createTaskItem(title, description, assignee, completed = false) {
+  const taskItem = document.createElement("li");
+  taskItem.innerHTML = `
+      <strong>${title}</strong>
+      <p>${description}</p>
+      <p>Assignee: ${assignee}</p>
+      <div class="task-buttons">
+          <button class="completeBtn">${
+            completed ? "Completed" : "Mark as Completed"
+          }</button>
+          <button class="deleteBtn">Delete</button>
+      </div>
+  `;
+  taskItem.classList.toggle("completed", completed);
+  return taskItem;
+}
+function markTaskAsCompleted(taskItem) {
+  taskItem.classList.toggle("completed");
+  const completeBtn = taskItem.querySelector(".completeBtn");
+  completeBtn.textContent = taskItem.classList.contains("completed")
+    ? "Completed"
+    : "Mark as Completed";
+}
+
+// Function to delete a task
+function deleteTask(taskItem) {
+  taskItem.remove();
+}
+taskList.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.tagName === "BUTTON" && target.classList.contains("completeBtn")) {
+    const taskItem = target.closest("li");
+    markTaskAsCompleted(taskItem);
+  } else if (
+    target.tagName === "BUTTON" &&
+    target.classList.contains("deleteBtn")
+  ) {
+    const taskItem = target.closest("li");
+    deleteTask(taskItem);
+  }
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const tasks = getTasksFromLocalStorage();
+  tasks.forEach((task) =>
+    addTaskToList(task.title, task.description, task.assignee, task.completed)
+  );
 });
