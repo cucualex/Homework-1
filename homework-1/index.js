0f34b8116d39b742d97c80a26cdbe1c480875f09
cucualex <cucualexandru15@yahoo.com>
2023-07-25 16:51:30 +0300

    Update(styling form, background, localStorage)
    localStorage store datas, but don't delete them, just hide, need more styling adjustements for items list

--- homework-1/index.js
+++ homework-1/index.js
@@ -3,6 +3,13 @@ const taskFormDialog = document.getElementById("taskFormDialog");
 const taskForm = document.getElementById("taskForm");
 const taskList = document.getElementById("taskList");
 
+// Load tasks from localStorage when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+  const tasks = getTasksFromLocalStorage();
+  tasks.forEach((task) =>
+    addTaskToList(task.title, task.description, task.assignee)
+  );
+});
 addTaskButton.addEventListener("click", () => {
   taskForm.reset();
   taskFormDialog.showModal();
@@ -20,8 +27,13 @@ taskForm.addEventListener("submit", (event) => {
     taskDescription.trim() !== "" &&
     assigneeEmail.trim() !== ""
   ) {
-    const taskItem = createTaskItem(taskTitle, taskDescription, assigneeEmail);
-    taskList.appendChild(taskItem);
+    const task = {
+      title: taskTitle,
+      description: taskDescription,
+      assignee: assigneeEmail,
+    };
+    addTaskToList(taskTitle, taskDescription, assigneeEmail);
+    saveTaskToLocalStorage(task);
     taskFormDialog.close();
   }
 });
@@ -30,15 +42,32 @@ document.getElementById("cancelBtn").addEventListener("click", () => {
   taskFormDialog.close();
 });
 
-function addTaskToList(title, description, assignee) {
+function addTaskToList(title, description, assignee, completed = false) {
   const taskItem = document.createElement("li");
   taskItem.innerHTML = `
+  
+    
       <strong>${title}</strong>
       <p>${description}</p>
-      <p>Assignee: ${assignee}</p>
+      <p> ${assignee}</p>
+      
+      <div class="task-buttons">
+        <button class="completeBtn">&#9989</button>
+        <button class="deleteBtn">&#10060</button>
+      </div>
   `;
-  taskList.appendChild(taskItem);
+
+  if (completed) {
+    taskItem.classList.add("completed");
+  }
+
+  if (completed) {
+    completedTaskList.appendChild(taskItem);
+  } else {
+    taskList.appendChild(taskItem);
+  }
 }
+
 function saveTaskToLocalStorage(task) {
   const tasks = getTasksFromLocalStorage();
   tasks.push(task);
@@ -49,50 +78,60 @@ function getTasksFromLocalStorage() {
   const tasksJSON = localStorage.getItem("tasks");
   return tasksJSON ? JSON.parse(tasksJSON) : [];
 }
-function createTaskItem(title, description, assignee, completed = false) {
-  const taskItem = document.createElement("li");
-  taskItem.innerHTML = `
-      <strong>${title}</strong>
-      <p>${description}</p>
-      <p>Assignee: ${assignee}</p>
-      <div class="task-buttons">
-          <button class="completeBtn">${
-            completed ? "Completed" : "Mark as Completed"
-          }</button>
-          <button class="deleteBtn">Delete</button>
-      </div>
-  `;
-  taskItem.classList.toggle("completed", completed);
-  return taskItem;
-}
+taskList.addEventListener("click", (event) => {
+  const targetElement = event.target;
+  if (targetElement.classList.contains("completeBtn")) {
+    markTaskAsCompleted(targetElement.closest("li"));
+  } else if (targetElement.classList.contains("deleteBtn")) {
+    deleteTask(targetElement.closest("li"));
+  }
+});
+
 function markTaskAsCompleted(taskItem) {
   taskItem.classList.toggle("completed");
-  const completeBtn = taskItem.querySelector(".completeBtn");
-  completeBtn.textContent = taskItem.classList.contains("completed")
-    ? "Completed"
-    : "Mark as Completed";
-}
+  const taskTitle = taskItem.querySelector("strong").textContent;
+  const taskDescription = taskItem.querySelector("p").textContent;
+  const assignee = taskItem
+    .querySelector("p:last-child")
+    .textContent.replace("Assignee: ", "");
 
+  const completedTask = {
+    title: taskTitle,
+    description: taskDescription,
+    assignee: assignee,
+  };
+
+  // Remove the completed task from the main task list and add it to the completedTaskList
+  taskList.removeChild(taskItem);
+  addTaskToList(
+    completedTask.title,
+    completedTask.description,
+    completedTask.assignee,
+    true
+  );
+  saveTaskToLocalStorage(completedTask, true);
+}
+function deleteTask(taskItem) {
+  const taskTitle = taskItem.querySelector("strong").textContent;
+  const taskDescription = taskItem.querySelector("p").textContent;
+  const assignee = taskItem
+    .querySelector("p:last-child")
+    .textContent.replace("Assignee: ", "");
+
+  const task = {
+    title: taskTitle,
+    description: taskDescription,
+    assignee: assignee,
+  };
+
+  // Remove the task from the main task list
+  taskList.removeChild(taskItem);
+  // Save the task to localStorage for any undo/redo functionalities, if needed
+  saveTaskToLocalStorage(task);
+}
 // Function to delete a task
 function deleteTask(taskItem) {
   taskItem.remove();
+  // localStorage.removeItem(taskItem);
+  sessionStorage.removeItem(taskItem);
 }
-taskList.addEventListener("click", (event) => {
-  const target = event.target;
-  if (target.tagName === "BUTTON" && target.classList.contains("completeBtn")) {
-    const taskItem = target.closest("li");
-    markTaskAsCompleted(taskItem);
-  } else if (
-    target.tagName === "BUTTON" &&
-    target.classList.contains("deleteBtn")
-  ) {
-    const taskItem = target.closest("li");
-    deleteTask(taskItem);
-  }
-});
-document.addEventListener("DOMContentLoaded", () => {
-  const tasks = getTasksFromLocalStorage();
-  tasks.forEach((task) =>
-    addTaskToList(task.title, task.description, task.assignee, task.completed)
-  );
-});
